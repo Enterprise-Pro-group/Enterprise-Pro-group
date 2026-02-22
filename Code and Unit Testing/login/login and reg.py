ae947e00df2c239c2e9d1d696728f708346252ac
MaryamH2704 <mharoon2704@gmail.com>
2026-02-22 16:42:33 +0000

    Configuring login front end and backend

---
+++ Code and Unit Testing/login/login and reg.py
@@ -0,0 +1,62 @@
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+
+
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = %s AND password = %s', (email, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['user_id'] = account['user_id']
+            session['email'] = account['email']
+            return render_template('index.html', msg='Logged in successfully!')
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('Login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('user_id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form :
+        email = request.form.get['email']
+        password = request.form.get['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE email = %s', (email,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+
+        elif  not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', ( password, email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    return render_template('Signup.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
